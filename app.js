10c491031e5b9a9bce5d4c3695a136c56d3d9c2d
gamesworkin <gamesworkin@gmail.com>
2026-06-03 09:18:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -847,7 +847,7 @@ function setupEventListeners() {
     }
     if (document.getElementById('btn-logout')) document.getElementById('btn-logout').onclick = (e) => { e.preventDefault(); handleLogoutActions(); };
     
-        // MAPEAMENTO REATIVO DOS BOTÕES SWITCH DE TEMAS E ACABAMENTOS
+    // MAPEAMENTO REATIVO DOS BOTÕES SWITCH DE TEMAS E ACABAMENTOS
     ['youtube', 'netflix', 'futurista', 'claro'].forEach(tema => {
         const btn = document.getElementById(`theme-switch-${tema}`);
         if (btn) {
@@ -856,13 +856,10 @@ function setupEventListeners() {
                 document.body.className = className;
                 if(currentUser) localStorage.setItem(`streamhub_layout_mode_${currentUser}`, className);
             };
-        }
-    });
-
-    } //
+    }
 
     configurarEventosBuscaCanal(); inicializarSeletorCoresLinear();
-} 
+}
 
 // Inicialização segura dos manipuladores nativos
 configurarEventosLogin(); checkSession();
